96abbae08ac6cfca56be5bdcf407bdcdecd99bdc
Chia1104 <yuyuchia7423@gmail.com>
2022-07-21 16:36:45 +0800

    :test_tube: Testing.

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 8080);
+  await app.listen(process.env.NODE_ENV === 'production' ? 8080 : 3001);
 }
 bootstrap().then(() =>
-  console.log(`Server is running on port ${process.env.PORT || 8080}`),
+  console.log(
+    `Server is running on port ${
+      process.env.NODE_ENV === 'production' ? 8080 : 3001
+    }`,
+  ),
 );
